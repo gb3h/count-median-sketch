5c177dc917fbe32e84a526c12bba2c8394c98b39
Gabriel Yeo <gabriel.yfy@gmail.com>
2021-09-28 16:29:23 +0800

    Init test suite

--- code/one.ml
+++ code/one.ml
@@ -5,7 +5,7 @@ let () = Random.init 5234
 
 let generate_hashes m p k =
   let prime_field_hash a b i = ((a * i) + b) % p % m in
-  let ab_list = List.init k ~f:(fun _ -> Random.int (p - 1), Random.int (p - 1)) in
+  let ab_list = List.init k ~f:(fun _ -> Random.int_incl 1 (p - 1), Random.int (p - 1)) in
   let hashes = List.map ab_list ~f:(fun (a, b) -> prime_field_hash a b) in
   hashes
 ;;
--- code/tests.ml
+++ code/tests.ml
@@ -1,9 +1,17 @@
 open Assert
 open One
+
+let hashes = generate_hashes 5 19 3
+let first_hash = List.hd hashes
+
 let tests : suite =
   [ Test
-      ( "Test test"
-      , [ "case1", assert_eqf (fun () -> 42) 42
-        ; "case2", assert_eqf (fun () -> Int.of_float 5.) 5
+      ( "Test hash of 0 to 4"
+      , [ "case1", assert_eqf (fun () -> first_hash 1) 4
+        ; "case2", assert_eqf (fun () -> first_hash 1) 4
+        ; "case3", assert_eqf (fun () -> first_hash 98192832) 0
+        ; "case4", assert_eqf (fun () -> first_hash 8312) 0
+        ; "case5", assert_eqf (fun () -> first_hash 19723) 4
         ] )
   ]
+;;
