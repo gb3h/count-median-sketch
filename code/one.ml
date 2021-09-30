fe18a434a026c3199a85060e8c39897cb4786a67
Gabriel Yeo <gabriel.yfy@gmail.com>
2021-09-30 12:54:32 +0800

    Add multiple query harness

--- code/one.ml
+++ code/one.ml
@@ -3,13 +3,9 @@ open Code.Hashutil
 
 let tuple_of_input a b c d e = a, b, c, d, e
 
-let num_hashes, end_range, max_value, file, x =
+let num_hashes, end_range, max_value, file_prefix, x =
   Scanf.scanf "%d %d %d %s %d" tuple_of_input
 ;;
-
-let hashes = generate_hashes ~end_exclusive:end_range num_hashes |> Array.of_list
-let real_counts = Array.init max_value ~f:(fun _ -> 0)
-let counters = Array.init num_hashes ~f:(fun _ -> Array.init end_range ~f:(fun _ -> 0))
 
 let middle arr =
   let len = Array.length arr in
@@ -21,33 +17,36 @@ let middle arr =
   else arr.(mid) |> Float.of_int
 ;;
 
-let algorithm_one_query x =
-  let arr = Array.init num_hashes ~f:(fun i -> counters.(i).(hashes.(i) x)) in
-  Array.sort arr ~compare:Int.compare;
-  middle arr
-;;
-
-let algorithm_two_query x =
-  let estimate x i =
-    let jay = hashes.(i) x in
-    let neighbour =
-      if jay % 2 = 0 then counters.(i).(jay + 1) else counters.(i).(jay - 1)
-    in
-    let value = counters.(i).(jay) in
-    value - neighbour
-  in
-  let arr = Array.init num_hashes ~f:(fun i -> estimate x i) in
-  Array.sort arr ~compare:Int.compare;
-  middle arr
-;;
-
 let print_counters counters =
   Array.iter counters ~f:(fun arr ->
       Array.iter arr ~f:(printf "%d ");
       print_endline "")
 ;;
 
-let () =
+let run_on_file file =
+  let hashes = generate_hashes ~end_exclusive:end_range num_hashes |> Array.of_list in
+  let real_counts = Array.init max_value ~f:(fun _ -> 0) in
+  let counters =
+    Array.init num_hashes ~f:(fun _ -> Array.init end_range ~f:(fun _ -> 0))
+  in
+  let algorithm_one_query x =
+    let arr = Array.init num_hashes ~f:(fun i -> counters.(i).(hashes.(i) x)) in
+    Array.sort arr ~compare:Int.compare;
+    middle arr
+  in
+  let algorithm_two_query x =
+    let estimate x i =
+      let jay = hashes.(i) x in
+      let neighbour =
+        if jay % 2 = 0 then counters.(i).(jay + 1) else counters.(i).(jay - 1)
+      in
+      let value = counters.(i).(jay) in
+      value - neighbour
+    in
+    let arr = Array.init num_hashes ~f:(fun i -> estimate x i) in
+    Array.sort arr ~compare:Int.compare;
+    middle arr
+  in
   In_channel.with_file file ~f:(fun ic ->
       In_channel.iter_lines ic ~f:(fun line ->
           let line = String.strip line in
@@ -57,13 +56,21 @@ let () =
               let output = hash n in
               let counter = Array.get counters id in
               counter.(output) <- counter.(output) + 1)));
-  let actual_count = real_counts.(x) in
-  let algo_one_res = algorithm_one_query x |> Float.round_nearest |> Int.of_float in
-  let algo_two_res = algorithm_two_query x |> Float.round_nearest |> Int.of_float in
-  printf "x: %d\n" x;
-  printf "real freq: %d\n" actual_count;
-  printf "algo 1 query: %d\n" algo_one_res;
-  printf "algo 1 diff: %d\n" (abs (actual_count - algo_one_res));
-  printf "algo 2 query: %d\n" algo_two_res;
-  printf "algo 2 diff: %d\n" (abs @@ (actual_count - algo_two_res))
+  real_counts, algorithm_one_query, algorithm_two_query
 ;;
+
+let run_multiple_queries file queries =
+  let real_counts, algorithm_one_query, algorithm_two_query = run_on_file file_prefix in
+  List.iter queries ~f:(fun x ->
+      let actual_count = real_counts.(x) in
+      let algo_one_res = algorithm_one_query x |> Float.round_nearest |> Int.of_float in
+      let algo_two_res = algorithm_two_query x |> Float.round_nearest |> Int.of_float in
+      printf "x: %d\n" x;
+      printf "real freq: %d\n" actual_count;
+      printf "algo 1 query: %d\n" algo_one_res;
+      printf "algo 1 diff: %d\n" (abs (actual_count - algo_one_res));
+      printf "algo 2 query: %d\n" algo_two_res;
+      printf "algo 2 diff: %d\n" (abs @@ (actual_count - algo_two_res)))
+;;
+
+let () = run_multiple_queries file_prefix [ x; 12 ]
